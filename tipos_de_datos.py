4916eac9ec7b5dc801092e37a5712dbd60163b87
Alejandro Caicedo Cajiao <alejandro.cajiao0525@gmail.com>
2024-07-16 15:22:35 -0500

    se inicializa el repositorio de python

---
+++ tipos_de_datos.py
@@ -0,0 +1,14 @@
+"string" #se utiliza para cadenas de texto
+'string'
+"""string"""
+'''string'''
+"""Tus datos son 
+   nombre: Alejandro
+   apellido: Caicedo"""
+#tipos de datos numericos
+4#tipos de datos entero
+4.5#tipos de datos flotante
+
+#tipos de datos booleanos
+True#True va con T mayuscula
+False#False va con F mayuscula
