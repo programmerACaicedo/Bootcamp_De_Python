fc4871ea074e78193feffc3af17020dcc54cd21b
Alejandro Caicedo Cajiao <alejandro.cajiao0525@gmail.com>
2024-07-16 16:22:25 -0500

    Avanzamos Variables

---
+++ variables/variables.py
@@ -0,0 +1,14 @@
+nombre = "Alejandro Caicedo"
+bienvenido = "Hola " + nombre + " Feliz Noches"
+
+
+print(bienvenido)
+print("ola"in bienvenido)
+print("Santiago" not in bienvenido)
+print("Hola" not in bienvenido)
+
+#CamelCase
+nombrecompletoDeTuProfe = "jheyson Galvis"
+
+#Snake Case es la que se debe usar en Python
+nombre_completo_De_Tu_Profe = "jheyson Galvis"
