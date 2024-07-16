ef3d2971384d1f7632f82f698fe5106c87937529
Alejandro Caicedo Cajiao <alejandro.cajiao0525@gmail.com>
2024-07-16 16:42:35 -0500

    se abordaron fundamentos de python

---
+++ Fundamentos.py
@@ -0,0 +1,18 @@
+name="             Alejandro            "
+print (type (name))
+print (name [0])
+print (name [1])
+print (name [2])
+print (name [3])
+print (name [4])
+print (name [5])
+print (name [6])
+print (name [7])
+print (name [8])
+last_name="Caicedo"
+print (name*5)
+print (len (name))
+print(name.lower())
+print(name.upper())
+print(name.strip())
+print(name + " " + last_name)
