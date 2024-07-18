abcb31cf5fef261653d647b7cb822556493fac74
Alejandro Caicedo Cajiao <alejandro.cajiao0525@gmail.com>
2024-07-17 20:48:32 -0500

    abordamos operaciones matematicas

---
+++ operaciones_matematica.py
@@ -0,0 +1,48 @@
+#variables
+a = 10
+b = 3
+
+# operaciones matematicas basicas
+
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+print(a == b)
+print(a != b)
+
+# operadores aritméticos
+
+print("Suma: ", a+b)
+print("Resta: ", a-b)
+print("Multiplicación: ", a*b)
+print("División: ", a/b)
+
+# en python el operador (%) obtiene el residuo de una division
+print("Residuo: ", a%b)
+# el siguiente signo es el doble división y el resultado seria la parte entera
+print("División entera: ", a//b)
+#potencia
+print("Potencia: ", a**b)
+a += 2
+print(a)
+a *= 2
+print(a)
+a /= 2
+print(a)
+a -= 2
+print(a)
+
+operacion_1 = 2 + 3* 4
+print(operacion_1)
+operacion_2 = 2 + (3* 4)
+print(operacion_2)
+operacion_3 = (2 + 3) * 4
+print(operacion_3)
+operacion_4 = (2 + 3) * (4** 2) / 8 - 1
+
+
+
+
+
+
