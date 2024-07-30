b1a25cbf5a28806519a95e5792be0c6662c7c925
Alejandro Caicedo <alejandro.cajiao0525@gmail.com>
2024-07-29 20:15:10 -0500

    se agregaron operadores aritmeticos

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,21 @@
+#Restas y Sumas
+suma= 2024+2000
+print (suma)
+resta= 2024-2000
+print(resta)
+multiplicacion = 23 * 7
+print(multiplicacion)
+division_flotante = 8 / 2
+print (division_flotante)
+# Potenciacion
+potencia = 2**3
+print(potencia)
+# Division Entera
+division_entera = 8 / 2
+print(division_entera)
+#Resto(
+resto = 23 % 7 
+print(resto)
+print(suma, resta, multiplicacion)
+tipo_de_dato = type(division_entera)
+print(tipo_de_dato)
