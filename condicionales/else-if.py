8786648484f2750902c2a614d2640a373df746da
Alejandro Caicedo <alejandro.cajiao0525@gmail.com>
2024-07-29 21:01:05 -0500

    se abordaron condicionales

---
+++ condicionales/else-if.py
@@ -0,0 +1,26 @@
+ingreso_mensual = 12000
+ahorro_mensual = 6000
+
+if ingreso_mensual > 10000:
+   if ahorro_mensual < 7000:
+      print("Ahora si estas bien")
+   else:
+      print("estas gastando mucho,ahorra mas")
+
+elif ingreso_mensual > 1000:
+ print("estás bien latinoamérica")
+
+elif ingreso_mensual > 500:
+  print("estas bien en colombia")
+
+elif ingreso_mensual > 200:
+ print("estas bien en peru")
+
+elif ingreso_mensual > 100:
+  print("estas bien en chile")
+
+elif ingreso_mensual > 50:
+  print("estas bien en venezuela")
+
+else:
+ print("estás en la pobreza")
---
+++ condicionales/if-else.py
@@ -0,0 +1,6 @@
+contrasena_almacenada = "JheysonGalvis"
+contrasena_escrita = "JheysonGalvis"
+if contrasena_almacenada == contrasena_escrita:
+   print ("hay coincidencia")
+else:
+   print ("No hay conincidencia")
