909fbf103a14da3954a53c20e7e68343be81ceb1
Alejandro Caicedo <alejandro.cajiao0525@gmail.com>
2024-08-05 20:59:38 -0500

    se abordaron listas de listas °_°

---
+++ lista_de_lista.py
@@ -0,0 +1,10 @@
+matrix = [[1, 2, 3], 
+          [4, 5, 6], 
+          [7, 8, 9]]
+print(matrix)
+print(matrix[0])
+number =(1, 2, 3, 4, 5,)
+print(number)
+print(type(number))
+print(number[0])
+#numbers = 10
